4cc15052154963b73bfdbce23a50a45fb46c7301
gonzalorportfolio <gromeroeducation@gmail.com>
2025-01-23 12:57:14 -0500

    update

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,15 +1,29 @@
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+//   // Keep the forward slashes / / around your repo name
+//   build: {
+//     outDir: "..",
+//     rollupOptions: {
+//       input: {
+//         main: "index.html",
+//         page1: "./src/other.html",
+//       },
+//     },
+//   },
+//   base: '/multi-page-test/', 
+// });
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  // Keep the forward slashes / / around your repo name
+  base: '/multi-page-test/', // Replace <REPOSITORY_NAME> with your GitHub repo name
   build: {
-    outDir: "..",
     rollupOptions: {
       input: {
-        main: "index.html",
-        page1: "./src/other.html",
+        home: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, './src/other.html'),
       },
     },
   },
-  base: '/multi-page-test/', 
-});
+})
